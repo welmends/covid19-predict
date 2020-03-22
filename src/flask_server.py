21e57002d50748475596e09116a04691b606601e
well <wellmend0@gmail.com>
2020-03-22 16:31:27 -0300

    add remaining parameters

--- src/flask_server.py
+++ src/flask_server.py
@@ -1,89 +1,8 @@
-import numpy as np
-import pandas as pd
-from scipy.integrate import odeint
+from seir_prediction import predict
 import json
 import flask
 from flask import request, jsonify
 from flask import Flask
-
-
-def seir(y, t, b, a, g, p, u, N):
-    dy = [0, 0, 0, 0, 0, 0]
-    S = N-sum(y)
-    dy[0] = np.dot(b[1:3], y[1:3])*S-a*y[0]  # E
-    dy[1] = a*y[0]-(g[1]+p[1])*y[1]  # I1
-    dy[2] = p[1]*y[1] - (g[2]+p[2])*y[2]  # I2
-    dy[3] = p[2]*y[2] - (g[3]+u)*y[3]  # I3
-    dy[4] = np.dot(g[1:3], y[1:3])  # R
-    dy[5] = u*y[3]  # D
-
-    return dy
-
-
-def predict(IP, DMI, FM, FS, FC, TMC, T_UTI_D, DH, CHNT, POP, TMAX):
-
-    # --- Parametros de entrada
-    # IP - Periodo de incubação em dias
-    IP = int(IP)
-    # DMI - Duracao de infecções leves em dias
-    DMI = float(DMI)
-    # FM - Fracao de infeccoes leves
-    FM = float(FM)
-    # FS - Fracao de infeccoes graves
-    FS = float(FS)
-    # FC - Fracao de infeccoes criticas
-    FC = float(FC)
-    # TMC - Taxa de mortalidade de casos (fracao de infeccoes resultando em morte)
-    TMC = float(TMC)
-    # T_UIT_D - Tempo de internacao na UTI ate a morte em dias
-    T_UTI_D = int(T_UTI_D)
-    # DH - Duracao da internacao no hospital em dias
-    DH = int(DH)
-    # CHNT - Casos hospitalizados nao transmitem
-    CHNT = int(1)
-    # POP - Tamanho da populacao
-    POP = int(POP)
-    # TMAX - Tempo de medicao em dias
-    TMAX = int(TMAX)
-
-    # Soma de FM, FS e FC deve ser igual a 1
-    if(FM+FS+FC != 1):
-        return -1
-
-    # --- Definir parametros e executar ODE
-    b = np.zeros(4)  # beta
-    g = np.zeros(4)  # gamma
-    p = np.zeros(3)
-    a = 1/IP
-    u = (1/T_UTI_D)*(TMC/FC)
-    g[3] = (1/T_UTI_D)-u
-    p[2] = (1/DH)*(FC/(FC+FS))
-    g[2] = (1/DH)-p[2]
-    g[1] = (1/DMI)*FM
-    p[1] = (1/DMI)-g[1]
-
-    if(CHNT != 0):
-        # casos hospitalizados nao transmitem
-        b = 2.5e-4*np.array([0, 1, 0, 0])
-    else:
-        # todas as etapas transmitem igualmente
-        b = 2.5e-4*np.array([1, 1, 1, 1])
-
-    # Calcula a taxa reprodutiva basica
-    R0 = POP*((b[1]/(p[1]+g[1]))+(p[1]/(p[1]+g[1])) *
-              (b[2]/(p[2]+g[2]) + (p[2]/(p[2]+g[2]))*(b[3]/(u+g[3]))))
-
-    # --- Aplicar predicao
-
-    tvec = np.arange(0, TMAX, 0.1)
-    ic = np.zeros(6)
-    ic[0] = 1
-
-    soln = odeint(seir, ic, tvec, args=(b, a, g, p, u, POP))
-    soln = np.hstack((POP-np.sum(soln, axis=1, keepdims=True), soln))
-
-    # soln -> [0:TMAX][S, E, I1, I2, I3, R, D]
-    return soln
 
 
 def get_predict_json(soln):
@@ -95,12 +14,11 @@ def get_predict_json(soln):
     return json_list
 
 
-################################## Flask Configs ###############################
 app = flask.Flask(__name__)
 app.config["DEBUG"] = False
 
 ################################## Flask Services ##############################
-# http://lapisco.fortaleza.ifce.edu.br:3012/api/covid19predict?IP=5&DMI=10&FM=0.8&FS=0.15&FC=0.05&TMC=0.02&T_UTI_D=7&DH=11&CHNT=1&POP=8843000&TMAX=365
+# http://lapisco.fortaleza.ifce.edu.br:3012/api/covid19predict?IP=5&DMI=10&FM=0.8&FS=0.15&FC=0.05&TMC=0.02&T_UTI_D=7&DH=11&B1=0.33&B2=0.01&B3=0.01&PII=1&POP=8843000&TMAX=365
 @app.route('/api/covid19predict', methods=['GET'], endpoint='apicovid19predict')
 def apicovid19predict():
     # params
@@ -112,17 +30,21 @@ def apicovid19predict():
     TMC = request.args.get('TMC')
     T_UTI_D = request.args.get('T_UTI_D')
     DH = request.args.get('DH')
-    CHNT = request.args.get('CHNT')
+    B1 = request.args.get('B1')
+    B2 = request.args.get('B2')
+    B3 = request.args.get('B3')
+    PII = request.args.get('PII')
     POP = request.args.get('POP')
     TMAX = request.args.get('TMAX')
 
     # return if has no arguments
-    if(IP == None or DMI == None or FM == None or FS == None or FC == None or TMC == None or T_UTI_D == None or DH == None or CHNT == None or POP == None or TMAX == None):
+    if(IP == None or DMI == None or FM == None or FS == None or FC == None or TMC == None or T_UTI_D == None or DH == None or B1 == None or B2 == None or B3 == None or PII == None or POP == None or TMAX == None):
         return '<h2 style="text-align: center;"><strong>covid19predict (routes)</strong></h2><ul><li><strong>predict - api/covid19predict?</strong></li><li><strong>input information - <a href="http://lapisco.fortaleza.ifce.edu.br:3012/api/covid19predict/input">api/covid19predict/input</a></strong></li><li><strong>output information - <a href="http://lapisco.fortaleza.ifce.edu.br:3012/api/covid19predict/output">api/covid19predict/output</a></strong></li></ul>'
 
     # predict
-    #slon = predict(5, 10, 0.8, 0.15, 0.05, 0.02, 7, 11, 1, 8843000, 365)
-    slon = predict(IP, DMI, FM, FS, FC, TMC, T_UTI_D, DH, CHNT, POP, TMAX)
+    #slon = predict(5, 10, 0.8, 0.15, 0.05, 0.02, 7, 11, 0.33, 0.01, 0.01, 1, 8843000, 365)
+    slon = predict(IP, DMI, FM, FS, FC, TMC, T_UTI_D,
+                   DH, B1, B2, B3, PII, POP, TMAX)
 
     # json
     json_list = get_predict_json(slon)
---
+++ src/seir_prediction.py
@@ -0,0 +1,113 @@
+import numpy as np
+import pandas as pd
+from scipy.integrate import odeint
+
+
+def taxa_reprodutiva(N, b, p, g, u):
+    return N*b[0]/(p[1]+g[0]) + (p[0]/(p[0]+g[0]))*(N*b[1]/(p[1]+g[1]) + N*b[2]*p[1]/((p[1]+g[1])*(u + g[2])))
+
+
+def params(g, p, IncubPeriod, FracMild, FracCritical, FracSevere, TimeICUDeath, CFR, DurMildInf, DurHosp, tmax, i):
+
+    a = 1/IncubPeriod  # Taxa de transição dos expostos para infectado
+
+    if FracCritical == 0:
+        u = 0
+    else:
+        u = (1/TimeICUDeath)*(CFR/FracCritical)
+
+    g[0] = (1/DurMildInf)*FracMild  # Taxa de recuperação I1
+    p[0] = (1/DurMildInf)-g[0]  # Taxa de progreção I1
+
+    g[2] = (1/TimeICUDeath)-u  # Taxa de recuperação I3
+
+    # Taxa de progressão I2
+    p[1] = (1/DurHosp)*(FracCritical/(FracCritical+FracSevere))
+    g[1] = (1/DurHosp)-p[1]  # Taxa de recuperação de I2
+
+    tvec = np.arange(0, tmax, 0.1)
+    # Inicia vetor da população (cada índice para cada tipo de infectado, exposto, etc)
+    ic = np.zeros(6)
+    ic[0] = i  # População sucetível = tamanho da população
+
+    return a, u, g, p, tvec, ic
+
+
+def seir(y, t, b, a, g, p, u, N):
+
+    dy = [0, 0, 0, 0, 0, 0]
+    S = N-sum(y)  # S -> casos sucetiveis
+    dy[0] = (b[0]*y[1] + b[1]*y[2] + b[2]*y[3]) * \
+        S - a*y[0]  # dE -> casos expostos
+    dy[1] = a*y[0] - (g[0] + p[0])*y[1]  # dI1 -> Casos leves
+    dy[2] = p[0]*y[1] - (g[1]+p[1])*y[2]  # dI2 -> Casos graves
+    dy[3] = p[1]*y[2] - (g[2] + u)*y[3]  # dI3 -> Casos Críticos
+    dy[4] = g[0]*y[1] + g[1]*y[2] + g[2]*y[3]  # dR -> Recuperados
+    dy[5] = u*y[3]  # dD -> Mortos
+
+    return dy
+
+
+def growth_rate(tvec, soln, t1, t2, i):
+
+    i1 = np.where(tvec == t1)[0][0]
+    i2 = np.where(tvec == t2)[0][0]
+    r = (np.log(soln[i2, 1])-np.log(soln[i1, 1]))/(t2-t1)
+    DoublingTime = np.log(2)/r
+
+    return r, DoublingTime
+
+
+def predict(IP, DMI, FM, FS, FC, TMC, T_UTI_D, DH, B1, B2, B3, PII, POP, TMAX):
+    # --- Parametros de entrada
+    # IP - Periodo de incubação em dias
+    IP = int(IP)
+    # DMI - Duracao de infecções leves em dias
+    DMI = float(DMI)
+    # FM - Fracao de infeccoes leves
+    FM = float(FM)
+    # FS - Fracao de infeccoes graves
+    FS = float(FS)
+    # FC - Fracao de infeccoes criticas
+    FC = float(FC)
+    # TMC - Taxa de mortalidade de casos (fracao de infeccoes resultando em morte)
+    TMC = float(TMC)
+    # T_UIT_D - Tempo de internacao na UTI ate a morte em dias
+    T_UTI_D = int(T_UTI_D)
+    # DH - Duracao da internacao no hospital em dias
+    DH = int(DH)
+    # B1 -
+    B1 = float(B1)
+    # B2 -
+    B2 = float(B2)
+    # B3 -
+    B3 = float(B3)
+    # PII - Pessoas inicialmente infectadas
+    PII = int(PII)
+    # POP - Tamanho da populacao
+    POP = int(POP)
+    # TMAX - Tempo de medicao em dias
+    TMAX = int(TMAX)
+
+    # --- Definir parametros e executar ODE
+    g = np.zeros(4)
+    p = np.zeros(3)
+
+    a, u, g, p, tvec, ic = params(
+        g, p, IP, FM, FC, FS, T_UTI_D, TMC, DMI, DH, TMAX, PII)
+
+    # Inicia vetor de taxa de transmissão
+    B1 = B1/POP
+    B2 = B2*B1
+    B3 = B3*B1
+    b = np.array([B1, B2, B3])
+
+    # Calcula a taxa reprodutiva básica
+    R0 = taxa_reprodutiva(POP, b, p, g, u)
+
+    # Simulacao
+    soln = odeint(seir, ic, tvec, args=(b, a, g, p, u, POP))
+    soln = np.hstack((POP-np.sum(soln, axis=1, keepdims=True), soln))
+
+    # soln -> [0:TMAX][S, E, I1, I2, I3, R, D]
+    return soln
